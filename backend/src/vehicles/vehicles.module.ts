9098cb49b125238442c3d3724dd86b58c65fa8ec
Anthony <anthonylopes037@gmail.com>
2026-07-07 00:14:41 +0200

    feat: add VIN decoding feature and maintenance presets

--- backend/src/vehicles/vehicles.module.ts
+++ backend/src/vehicles/vehicles.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common'
 import { VehiclesController } from './vehicles.controller'
 import { VehiclesService } from './vehicles.service'
+import { VinDecoderService } from './vin-decoder.service'
 
 @Module({
   controllers: [VehiclesController],
-  providers: [VehiclesService],
+  providers: [VehiclesService, VinDecoderService],
 })
 export class VehiclesModule {}
